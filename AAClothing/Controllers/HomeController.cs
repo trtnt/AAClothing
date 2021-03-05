0b66119cd36618bda5591612e96bca85d31185f5
trtnt <42801247+trtnt@users.noreply.github.com>
2021-03-05 22:04:21 +0100

    all

--- AAClothing/Controllers/HomeController.cs
+++ AAClothing/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,7 +22,45 @@ namespace AAClothing.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            List<Productmodel> products = new List<Productmodel>();
+            DataSet sqlDataSet = new DataSet();
+
+            string connectionString = "Server=DESKTOP-JA9OFG0\\SQLEXPRESS;Database=AAClothing;Trusted_Connection=True;";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand("SELECT * FROM Product", connection))
+                using (SqlDataAdapter sqlDataAdapter = new SqlDataAdapter())
+                {
+                    sqlDataAdapter.SelectCommand = sqlCommand;
+                    sqlDataAdapter.Fill(sqlDataSet);
+                }
+            }
+
+            foreach (DataRow dr in sqlDataSet.Tables[0].Rows)
+            {
+                if (products.Where(p => p.Productid == (int)dr["Productid"]).ToList().Count == 0)
+                {
+                    Productmodel product = new Productmodel()
+                    {
+                        Productid = (int)dr["Productid"],
+                        Productnaam = dr["Productnaam"].ToString(),
+                        Productprijs = (double)dr["Productprijs"],
+                        
+                        //ProductImage = (byte[])dr["ProductImage"],
+
+                    };
+
+                    products.Add(product);
+                }
+                else
+                {
+                    //products.FirstOrDefault(id => id.ProductId == (long)dr["ProductId"]).ProductCategories.Add(dr["ProductCategoryName"].ToString());
+                }
+
+        }
+            return View(products);
         }
 
         public IActionResult Privacy()
---
+++ AAClothing/Models/Productmodel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AAClothing.Models
+{
+    public class Productmodel
+    {
+
+        public int Productid { get; set; }
+        public string Productnaam { get; set; }
+        public double Productprijs { get; set; }
+        public string Productbeschrijving { get; set; }
+        public byte[] ProductImage { get; set; }
+
+    }
+}
